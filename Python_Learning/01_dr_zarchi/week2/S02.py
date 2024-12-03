2beb1a6c0cf09638b10400ee022ee9227cab07f3
morteza karbasi <mortezakarbasi2001@gmail.com>
2024-12-03 20:29:34 +0330

    add week 2 code

---
+++ Python_Learning/01_dr_zarchi/week2/S02.py
@@ -0,0 +1,16 @@
+# talk about input and int function
+
+# factoril
+n = 6
+m = 1
+for i in range(1,n+1):
+    print(f"m = {m}, i = {i}")
+    m = m * i
+    print(f"factoril {i} = {m}")
+print(m) 
+# sum 
+n = 5 
+m = 0
+for i in range(1,n+1):
+    m = m + i
+print(m)
